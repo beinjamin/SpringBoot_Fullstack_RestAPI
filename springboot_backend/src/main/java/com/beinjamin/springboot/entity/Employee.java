7d2657006e1f25d0fdf2c34dd75239b7430820e1
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-20 20:52:59 +0100

    Gestion des annotation lombok

--- springboot_backend/src/main/java/com/beinjamin/springboot/entity/Employee.java
+++ springboot_backend/src/main/java/com/beinjamin/springboot/entity/Employee.java
@@ -1,5 +1,16 @@
 package com.beinjamin.springboot.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Employee {
 	private long id;
 	private String firstName;
