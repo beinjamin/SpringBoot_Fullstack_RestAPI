5cee2ab4425a7e09f7610340e6076012f15f6635
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-20 21:20:36 +0100

    Gestion api

--- springboot_backend/src/main/java/com/beinjamin/springboot/controller/EmployeeController.java
+++ springboot_backend/src/main/java/com/beinjamin/springboot/controller/EmployeeController.java
@@ -1,5 +1,24 @@
 package com.beinjamin.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.beinjamin.springboot.entity.Employee;
+import com.beinjamin.springboot.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeController {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@GetMapping("/employee")
+	public List<Employee> fetchEmployee(){
+		return employeeRepository.findAll();
+		
+	}
 
 }
