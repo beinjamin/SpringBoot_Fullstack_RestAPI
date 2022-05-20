e7ea913b8ab4a50052015a2919499b91da7118d1
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-20 21:30:12 +0100

    Gestion api et creation des entité physique

--- springboot_backend/src/main/java/com/beinjamin/springboot/SpringbootBackendApplication.java
+++ springboot_backend/src/main/java/com/beinjamin/springboot/SpringbootBackendApplication.java
@@ -1,13 +1,59 @@
 package com.beinjamin.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.beinjamin.springboot.entity.Employee;
+import com.beinjamin.springboot.repository.EmployeeRepository;
+
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+    
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		Employee employee1 = Employee.builder()
+				.firstName("Jaures")
+				.lastName("FOTSING")
+				.email("jauresbeinajmin@gmail.com")
+				.build();
+		
+		Employee employee2 = Employee.builder()
+				.firstName("Marc")
+				.lastName("Papimouss")
+				.email("marc@gmail.com")
+				.build();
+		
+		Employee employee3 = Employee.builder()
+				.firstName("Hugorisso")
+				.lastName("Ive")
+				.email("ive@gmail.com")
+				.build();
+		Employee employee4 = Employee.builder()
+				.firstName("JObinno")
+				.lastName("Karl")
+				.email("karl@gmail.com")
+				.build();
+		
+		employeeRepository.save(employee1);
+		employeeRepository.save(employee2);
+		employeeRepository.save(employee3);
+		employeeRepository.save(employee4);
+
+
+
+		
+		
 	}
 
 }
