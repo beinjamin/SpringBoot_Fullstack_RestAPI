c774ef6095f18bd69ae8f56fe97cecba5a0ec284
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-20 21:10:30 +0100

    Gestion l'interface employee

--- springboot_backend/src/main/java/com/beinjamin/springboot/repository/EmployeeRepository.java
+++ springboot_backend/src/main/java/com/beinjamin/springboot/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.beinjamin.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository{
+import com.beinjamin.springboot.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository <Employee, Long>{
 
 }
