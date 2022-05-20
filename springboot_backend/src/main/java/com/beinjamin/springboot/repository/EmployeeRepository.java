67bb0794a253315b316bf0e197dd602cac0a4646
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-20 21:09:40 +0100

    Finalisation avec entity et mise en place de l'interface

--- springboot_backend/src/main/java/com/beinjamin/springboot/repository/EmployeeRepository.java
+++ springboot_backend/src/main/java/com/beinjamin/springboot/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.beinjamin.springboot.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository{
 
 }
