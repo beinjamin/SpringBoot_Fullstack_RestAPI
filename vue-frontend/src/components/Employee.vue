cc995d0d4108ff8083fd1e113d84c5ded54bc38f
Jaures <jauresbeinajmin@gmail.com>
2022-07-10 12:42:59 +0100

    mise en place de l'interface

---
+++ vue-frontend/src/components/Employee.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class = "container">
+        <h1 class="text-center"> Employees List</h1>
+        <table class="table table-striped">
+            <thead>
+                
+
+            </thead> 
+</template>
+
+<script>
+
+</script>
+
+
+<style lang="less" scoped>
+
+</style>
